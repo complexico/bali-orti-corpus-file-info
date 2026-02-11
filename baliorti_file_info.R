fc049e8131cd5e9738a73ed5eb23940c01d28087
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2026-02-11 19:57:49 +0800

    First commit.

---
+++ baliorti_file_info.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(fs)
+
+year_folder <- dir(path = "G:/Other computers/My MacBook Air/Documents/Corpora/_corpusbali/Surat Kabar/Orti Bali", pattern = "^20",
+                   full.names = TRUE)
+
+corpus_file_info <- vector(mode = "list", length = length(year_folder))
+names(corpus_file_info) <- str_c("y_", basename(year_folder), sep = "")
+
+for (i in seq_along(year_folder)) {
+  
+  corpus_file_info[[i]] <- dir(year_folder[i], full.names = TRUE) |> 
+    map(file_info) |> 
+    list_rbind() |> 
+    mutate(year_folder = basename(year_folder[i]),
+           path = basename(path))
+  
+}
+
+corpus_file_info |> list_rbind() |> 
+  write_tsv("baliorti_file_info.tsv")
